2a417ab8f39d5b8f905d9b99b9d24281c3eacaa3
忘忧北萱草 <wybxc@qq.com>
2022-11-20 10:32:23 +0800

    feat: 切换到 abi3 以支持 py3.8~3.9

--- python/awr/__init__.py
+++ python/awr/__init__.py
@@ -1,3 +1,31 @@
+"""
+基于 [`ricq`] 包装，供 Python 使用的 QQ 无头客户端。
+
+更多信息请参考 [`login`] 和 [`client`] 模块。
+
+# Examples
+```python
+import awr
+import asyncio
+
+async def main():
+    ## 登录账号
+    client = await awr.Dynamic().login(12345678, "./bots")
+    ## 读取群列表
+    print(await client.get_group_list())
+    ## 保持连接
+    await client.alive()
+    
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    import sys
+    sys.exit(0)
+```
+
+[`ricq`]: https://docs.rs/ricq/latest/ricq/
+"""
+
 from . import awr as _awr
 
 _awr.init(_awr)
